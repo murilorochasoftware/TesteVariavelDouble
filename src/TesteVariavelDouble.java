6d9ed06871e8c0a1e0fe2b4aa67a3284627cfb91
Murilo Rocha <software@murilorocha.com.br>
2022-05-02 19:47:24 -0300

    Teste do uso de variável com o tipo de dados double em Java.

---
+++ src/TesteVariavelDouble.java
@@ -0,0 +1,9 @@
+public class TesteVariavelDouble {
+
+    public static void main(String[] args) {
+        double dezesseisDigitosDecimais = 0.1234567890123456;
+
+        System.out.printf("O valor com 16 dígitos decimais é: %.16f", dezesseisDigitosDecimais);
+
+    }
+}
